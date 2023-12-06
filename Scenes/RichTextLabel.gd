b3b9f486ba0b85dc23da1752a5db67fe4b583c18
YolaineDrouart <96011211+YolaineDrouart@users.noreply.github.com>
2023-12-06 17:52:21 +0900

    collision quasi fini

--- Scenes/RichTextLabel.gd
+++ Scenes/RichTextLabel.gd
@@ -2,6 +2,13 @@ extends RichTextLabel
 
 
 func _ready():
+	var image_file = "/tileset/bulle.png"
+	var image = Image.new()
+	var error = image.load(image_file)
+	if error == OK:
+		var texture = ImageTexture.new()
+		texture.create_from_image(image)
+		$RichTextLabel.add_image(texture)
 	hide()  # Mask le label au début
 	
 
