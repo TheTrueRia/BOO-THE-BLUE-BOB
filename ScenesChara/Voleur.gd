45d98afbb301c0758084e69a3ebbe00d7a644c21
Ariane <96746290+TheTrueRia@users.noreply.github.com>
2023-12-19 20:37:10 +0900

    ouais c'est moche

--- ScenesChara/Voleur.gd
+++ ScenesChara/Voleur.gd
@@ -13,3 +13,14 @@ func _ready():
 func _process(delta):
 	if(master.voleur_taken):
 		global_position = master.array_pos[10]
+		if(master.velocity.x==0 && master.velocity.y == 0):
+			animated_sprite.play("voleurCapeVole")
+		elif(master.velocity.x<=0 && master.velocity.y < 0):
+				animated_sprite.play("voleurDosRunLeft")
+		elif(master.velocity.x<=0 && master.velocity.y >= 0):
+				animated_sprite.play("voleurRunLeft")
+		elif(master.velocity.x>0 && master.velocity.y < 0):
+				animated_sprite.play("voleurDosRunRight")
+		else:
+			animated_sprite.play("voleurRunRight")
+
