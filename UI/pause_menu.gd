d2b2e56c3476d700ad828cd3c97aacc931b54309
Alexis Boulic <67315055+NagiAiza@users.noreply.github.com>
2023-12-24 09:07:04 +0100

    HA
    test mais ça marche aps

--- UI/pause_menu.gd
+++ UI/pause_menu.gd
@@ -1,17 +1,17 @@
 extends Control
 
-@export var game_manager : GameManager
+@export var scene_ville : SceneVille
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	hide()
-	game_manager.connect("toggle_game_paused", _on_game_manager_toggle_game_paused)
+	scene_ville.connect("toggle_game_paused", _on_scene_ville_toggle_game_paused)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 
-func _on_game_manager_toggle_game_paused(is_paused : bool):
+func _on_scene_ville_toggle_game_paused(is_paused : bool):
 	if(is_paused):
 		show()
 	else:
@@ -19,7 +19,7 @@ func _on_game_manager_toggle_game_paused(is_paused : bool):
 
 
 func _on_resume_button_pressed():
-	game_manager.game_paused = false
+	scene_ville.game_paused = false
 
 
 func _on_exit_button_pressed():
